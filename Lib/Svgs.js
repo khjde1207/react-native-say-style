b2cc49a4407421ffb6d244fa225f42dc587ac635
홍현준 <khjde1207@gractor.com>
2019-03-15 11:02:34 +0900

    .

--- Lib/Svgs.js
+++ Lib/Svgs.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import Svgs,{  Circle,  Ellipse,  G as g,  Text,  TSpan,  TextPath,  Path as p ,  Polygon,  Polyline,  Line,  Rect,  Use,  Image,  Symbol,  Defs,  LinearGradient,  RadialGradient,  Stop,  ClipPath,  Pattern,  Mask,} from 'react-native-svg';
-import { AnimatedSVGPath } from 'react-native-svg-animations';
-import * as Animatable from 'react-native-animatable';
+import Svgs,{  Circle,  Ellipse,  G as g,  Text,  TSpan,  TextPath,  Path ,  Polygon,  Polyline,  Line,  Rect,  Use,  Image,  Symbol,  Defs,  LinearGradient,  RadialGradient,  Stop,  ClipPath,  Pattern,  Mask,} from 'react-native-svg';
 import {SC } from './StyleConv'
 import  {rem} from './GlobalCss';
 let num  = Number(rem/15).toFixed(2);
@@ -10,7 +8,7 @@ const Svg = (props) => {
   var width = props.width;
   var height = props.height;
   viewBox = {};
-  if(props.viewBox){
+  if(props.viewBox){ 
     let arrTmpBox = props.viewBox.split(" ");
     if(arrTmpBox.length == 4){
       viewBox["viewBox"] = `${arrTmpBox[0]} ${arrTmpBox[1]} ${arrTmpBox[2]*num} ${arrTmpBox[3]*num}`
@@ -29,12 +27,12 @@ const G = (props) => {
   return React.createElement(g , {...props , scale : num, ...SC.cls(props.cls | {})}); 
 }
 
-const Path = (props) => {
-  if(props.ani){
-    return React.createElement(AnimatedSVGPath , {...props ,});   
-  }
-  return React.createElement(p , {...props ,}); 
-}
+// const Path = (props) => {
+//   // if(props.ani){
+//   //   return React.createElement(AnimatedSVGPath , {...props ,});   
+//   // }
+//   return React.createElement(P , {...props});   
+// }
 
 export {
  Svg,
