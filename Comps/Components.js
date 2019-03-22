f27d146e85a1f538f7857b652ba2eca2d8519593
홍현준 <khjde1207@gractor.com>
2019-03-22 11:15:11 +0900

    .

--- Comps/Components.js
+++ Comps/Components.js
@@ -21,8 +21,3 @@ export {
   Input,
   Picker,
 }
-// export{
-
-// }
-
-
--- Comps/Comps/Svgs.js
+++ Comps/Comps/Svgs.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
-import Logo from '../../Res/Svg/Logo'
+// import Logo from '../../Res/Svg/Logo'
 
-const SvgList = {Logo,}
+// const SvgList = {Logo,}
 
 export default  class  extends Component {
-  fnRef = (ref)=>this.comp = ref;
+  // fnRef = (ref)=>this.comp = ref;
 
-  render(){
-    var SvgObj = SvgList[this.props.name]
-    return <SvgObj  ref={this.fnRef} {...this.props} >{this.props.children}</SvgObj>;  
+  // render(){
+  //   var SvgObj = SvgList[this.props.name]
+  //   return <SvgObj  ref={this.fnRef} {...this.props} >{this.props.children}</SvgObj>;  
 
-  }
+  // }
 }
