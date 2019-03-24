9b958f5363dc8298e184008957dba9f56d57a2b4
khjde1207 <khjde1207@knou.ac.kr>
2019-03-24 22:03:04 +0900

    .

--- Comp/Comps/Flux.js
+++ Comp/Comps/Flux.js
@@ -13,133 +13,133 @@ import {  Scene     ,//as Scene_ ,
           ActionConst} from 'react-native-router-flux';
 import {SC}  from '../../Lib/StyleConv';
 import _  from 'lodash'; 
-//====================================
-let Scenea =  styled(Scene_).attrs((props)=>{
-  return SC.conv(props, {}); 
-})`
-${SC.cls}
-`; 
- 
-//====================================
-let Lightboxa =  styled(Lightbox_).attrs((props)=>{
-  return SC.conv(props, {});
-})`
-${SC.cls}
-`; 
- 
-// //=================================
-let Stacka =  styled(Stack_).attrs((props)=>{
-  return SC.conv(props, {});
-})`
-${SC.cls}
-`; 
-
-// //=================================
-let Drawera =  styled(Drawer_).attrs((props)=>{
-  return SC.conv(props, {});
-})`
-${SC.cls}
-`; 
-
-// //=================================
-let Modala =  styled(Modal_).attrs((props)=>{
-  return SC.conv(props, {});
-})`
-${SC.cls}
-`; 
-
-// //=================================
-let Tabsa =  styled(Tabs_).attrs((props)=>{
-  return SC.conv(props, {});
-})`
-${SC.cls}
-`; 
-
-// //=================================
-let Overlaya =  styled(Overlay_).attrs((props)=>{
-  return SC.conv(props, {});
-})`
-${SC.cls}
-`; 
-
-// //====================================
-let Routera =  styled(Router_).attrs((props)=>{
-  return SC.conv(props, {});
-})`
-${SC.cls}
-`; 
-
- 
-// //====================================
-
-class Lightbox extends Component {
-  
-  render(){
-    let tmpProps = Object.assign({},this.props); 
-    //SC.appendTheme(tmpProps,  this.props.th); 
-    return (<Lightbox_   {...this.props}>{this.props.children}</Lightbox_>)
-  }
-}
-
-class Drawer extends Component {
-  
-  render(){
-    let tmpProps = Object.assign({},this.props); 
-    //SC.appendTheme(tmpProps,  this.props.th);  
-    return (<Drawer_   {...this.props}>{this.props.children}</Drawer_>)
-  }
-}
-
-class Modal extends Component {
-  
-  render(){
-    let tmpProps = Object.assign({},this.props); 
-    //SC.appendTheme(tmpProps,  this.props.th); 
-    return (<Modala   {...this.props}>{this.props.children}</Modala>)
-  }
-}
-
-class Tabs extends Component {
-  
-  render(){
-    let tmpProps = Object.assign({},this.props); 
-    //SC.appendTheme(tmpProps,  this.props.th); 
-    return (<Tabsa   {...this.props}>{this.props.children}</Tabsa>)
-  }
-}
-
-class Overlay extends Component {
-  
-  render(){
-    let tmpProps = Object.assign({},this.props); 
-    //SC.appendTheme(tmpProps,  this.props.th); 
-    return (<Overlaya   {...this.props}>{this.props.children}</Overlaya>)
-  }
-}
-
-class Router extends Component {
-  render(){
-    let tmpProps = Object.assign({},this.props); 
-    //SC.appendTheme(tmpProps,  this.props.th); 
-    return (<Routera   {...this.props}>{this.props.children}</Routera>)
-  }
-}
-
-class Scene extends Component { 
-  render(){
-    let tmpProps = Object.assign({},this.props); 
-    //SC.appendTheme(tmpProps,  this.props.th); 
-    return (<Scenea   {...this.props}>{this.props.children}</Scenea>)
-  }
-}
- 
-class Stack extends Component {
-  render(){
-    let tmpProps = Object.assign({},this.props); 
-    //SC.appendTheme(tmpProps,  this.props.th); 
-    return (<Stacka   {...this.props}>{tmpProps.children}</Stacka>)
-  }
-}
  
 export {Scene  , Router , Stack ,  Overlay , Tabs , Modal , Drawer  ,Lightbox , Actions, ActionConst };  
+// //====================================
+// let Scenea =  styled(Scene_).attrs((props)=>{
+//   return SC.conv(props, {}); 
+// })`
+// ${SC.cls}
+// `; 
+ 
+// //====================================
+// let Lightboxa =  styled(Lightbox_).attrs((props)=>{
+//   return SC.conv(props, {});
+// })`
+// ${SC.cls}
+// `; 
+ 
+// // //=================================
+// let Stacka =  styled(Stack_).attrs((props)=>{
+//   return SC.conv(props, {});
+// })`
+// ${SC.cls}
+// `; 
+
+// // //=================================
+// let Drawera =  styled(Drawer_).attrs((props)=>{
+//   return SC.conv(props, {});
+// })`
+// ${SC.cls}
+// `; 
+
+// // //=================================
+// let Modala =  styled(Modal_).attrs((props)=>{
+//   return SC.conv(props, {});
+// })`
+// ${SC.cls}
+// `; 
+
+// // //=================================
+// let Tabsa =  styled(Tabs_).attrs((props)=>{
+//   return SC.conv(props, {});
+// })`
+// ${SC.cls}
+// `; 
+
+// // //=================================
+// let Overlaya =  styled(Overlay_).attrs((props)=>{
+//   return SC.conv(props, {});
+// })`
+// ${SC.cls}
+// `; 
+
+// // //====================================
+// let Routera =  styled(Router_).attrs((props)=>{
+//   return SC.conv(props, {});
+// })`
+// ${SC.cls}
+// `; 
+
+ 
+// // //====================================
+
+// class Lightbox extends Component {
+  
+//   render(){
+//     let tmpProps = Object.assign({},this.props); 
+//     //SC.appendTheme(tmpProps,  this.props.th); 
+//     return (<Lightbox_   {...this.props}>{this.props.children}</Lightbox_>)
+//   }
+// }
+
+// class Drawer extends Component {
+  
+//   render(){
+//     let tmpProps = Object.assign({},this.props); 
+//     //SC.appendTheme(tmpProps,  this.props.th);  
+//     return (<Drawer_   {...this.props}>{this.props.children}</Drawer_>)
+//   }
+// }
+
+// class Modal extends Component {
+  
+//   render(){
+//     let tmpProps = Object.assign({},this.props); 
+//     //SC.appendTheme(tmpProps,  this.props.th); 
+//     return (<Modala   {...this.props}>{this.props.children}</Modala>)
+//   }
+// }
+
+// class Tabs extends Component {
+  
+//   render(){
+//     let tmpProps = Object.assign({},this.props); 
+//     //SC.appendTheme(tmpProps,  this.props.th); 
+//     return (<Tabsa   {...this.props}>{this.props.children}</Tabsa>)
+//   }
+// }
+
+// class Overlay extends Component {
+  
+//   render(){
+//     let tmpProps = Object.assign({},this.props); 
+//     //SC.appendTheme(tmpProps,  this.props.th); 
+//     return (<Overlaya   {...this.props}>{this.props.children}</Overlaya>)
+//   }
+// }
+
+// class Router extends Component {
+//   render(){
+//     let tmpProps = Object.assign({},this.props); 
+//     //SC.appendTheme(tmpProps,  this.props.th); 
+//     return (<Routera   {...this.props}>{this.props.children}</Routera>)
+//   }
+// }
+
+// class Scene extends Component { 
+//   render(){
+//     let tmpProps = Object.assign({},this.props); 
+//     //SC.appendTheme(tmpProps,  this.props.th); 
+//     return (<Scenea   {...this.props}>{this.props.children}</Scenea>)
+//   }
+// }
+ 
+// class Stack extends Component {
+//   render(){
+//     let tmpProps = Object.assign({},this.props); 
+//     //SC.appendTheme(tmpProps,  this.props.th); 
+//     return (<Stacka   {...this.props}>{tmpProps.children}</Stacka>)
+//   }
+// }
 // export {Scene  , Router , Actions , Reducer , ActionConst, Overlay , Tabs , Modal , Drawer , Stack , Lightbox  }; 
